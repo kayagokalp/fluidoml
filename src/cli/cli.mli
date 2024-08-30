97563517977ec0477ff75769b4d6f0f10470c720
Kaya Gokalp <kayagokalp123@gmail.com>
2024-08-30 16:21:30 -0700

    chore: fmt

--- src/cli/cli.mli
+++ src/cli/cli.mli
@@ -1,9 +1,11 @@
 (** Entry point for the fluidoml CLI *)
 
-val parse_and_validate :
-           int ->
-           float ->
-           Input_space.t list -> (Config.checked_config, string) result
+val parse_and_validate
+  :  int
+  -> float
+  -> Input_space.t list
+  -> (Config.checked_config, string) result
+
 val run_cli : unit -> unit
 
 (* Expose the cmd value *)
